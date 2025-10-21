b17b7660aa6648416c1167e22bc3745dd7d7065d
Souid83 <swuidy@hotmail.fr>
2025-10-21 22:53:25 +0200

    f

---
+++ src/main.tsx
@@ -0,0 +1,17 @@
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import App from './App.tsx';
+import './index.css';
+import { setupFirstAdmin } from './lib/supabase.ts';
+
+// Try to set up first admin user
+setupFirstAdmin().catch(console.error);
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </StrictMode>
+);
